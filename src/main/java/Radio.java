943dff28a1cd659b2fb8fa441e57c39fe5bc155e
Suufer <lyapota231@gmail.com>
2023-03-11 19:35:48 +0300

    Commit

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -50,4 +50,15 @@ public class Radio {
         }
     }
 
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume++;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume--;
+        }
+    }
 }
